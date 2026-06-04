070f47c543fcf93bb73d299008d6c54df177fcc4
Andrzej Mikulski <andrzejmikulski@mini-andrzej.home>
2026-06-04 21:12:12 +0200

    update

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    turbo: {
-      rules: {
-        "*.ts": ["tsc --noEmit"],
-      },
-    },
-  },
 };
 
 module.exports = nextConfig;
